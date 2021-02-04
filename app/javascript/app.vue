20e65aca9c01120a52a0b61d476c901752d22cf9
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-02-04 18:11:07 +0900

    install vue

---
+++ app/javascript/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello World!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+}
+</style>
